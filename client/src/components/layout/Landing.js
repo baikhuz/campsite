c645b2aceb8d3de5c9eafef5a33bec143eed791d
Ilya Baykhuzin <baikhuz@gmail.com>
2019-06-24 23:05:00 -0400

    initialized front end, imported the stylesheets

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">CampSite</h1>
+          <p className="lead">
+            A Social Network for Coding Bootcamp Grads <br /> Create a profile,
+            share experiences, meet other bootcampers!
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">
+              Sign Up
+            </a>
+            <a href="login.html" className="btn btn-light">
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
